8efa922380383341ca92609b24cc6975324a1eee
NAVEEN001138 <naveenravi600@gmail.com>
2026-01-12 22:42:31 +0530

    Dev A changed bharath1.py

--- bharath1.py
+++ bharath1.py
@@ -1,7 +1,6 @@
 num1 = 10
-num2 = 27
+num2 = 20
 
 sum = num1 + num2
-print(f"The sum of {num1} and {num2} is: {sum}")
-print("be the god")
-print("praise the fool")
+print(sum)
+print("change from dev A")
